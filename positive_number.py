8f38e69077129e3007d9e42f1608b922d5a200dd
Adarsh arun <95844109+Adarsh-Arun@users.noreply.github.com>
2024-02-11 00:50:14 +0530

    Add files via upload

---
+++ positive_number.py
@@ -0,0 +1,11 @@
+def print_positive_numbers(numbers):
+    for num in numbers:
+        if num > 0:
+            print(num, end=' ')
+print_positive_numbers([12, -7, 5, 64, -14])
+
+def print_positive_numbers_2(numbers):
+    for num in numbers:
+        if num > 0:
+            print(num, end=' ')
+print_positive_numbers_2([12, 14, -95, 3])
