62af52f2c2545fb061b5acd8f5d9b0b840c83bad
Adarsh arun <95844109+Adarsh-Arun@users.noreply.github.com>
2024-02-11 00:31:13 +0530

    Add files via upload
    fibonacci series number in python upto 20

---
+++ fibonacci.py
@@ -0,0 +1,9 @@
+
+def fibonacci(n):
+    a, b = 0, 1
+    for _ in range(n):
+        print(a, end=' ')
+        a, b = b, a + b
+
+# Print the first 10 numbers in the Fibonacci series
+fibonacci(20)
